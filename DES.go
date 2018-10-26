fcbc3801b49232269a3734347df52d1256aa252e
liuyh73 <15989067460@163.com>
2018-10-26 21:41:34 +0800

    DES-Golang

--- DES.go
+++ DES.go
@@ -2,41 +2,30 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/liuyh73/DES-Golang/sipher_code"
 )
 
 // 10101010 11110000 11101011 11110111 00010101 00110011 10100101 01101010
 const clear_text = "1010101011110000111010111111011100010101001100111010010101101010"
-const initial_matrix_side = 8
+const initial_siphercode_side = 8
 
-var PC_2_matrix = [7][8]int{
-	{49, 41, 33, 25, 17, 9, 1},
-	{58, 50, 42, 34, 26, 18, 10},
-	{2, 59, 51, 43, 35, 27, 19},
-	{11, 3, 60, 52, 44, 36, 57},
-	{31, 23, 15, 7, 62, 54, 46, 38},
-	{30, 22, 14, 6, 61, 53, 45, 37},
-	{29, 21, 13, 5, 28, 20, 12, 4},
-}
+var sipherCode *sipher_code.Sipher_code
 
 func main() {
-	// for i := 0; i < initial_matrix_side; i++ {
-	// 	for j := 0; j < initial_matrix_side; j++ {
-	// 		fmt.Printf("%d ", initial_matrix[i][j])
-	// 	}
-	// 	fmt.Println()
-	// }
-
-	// initial replacement
 	clear_text_init_replace := ""
-	for i := 0; i < initial_matrix_side; i++ {
-		for j := 0; j < initial_matrix_side; j++ {
-			clear_text_init_replace += string(clear_text[initial_matrix[i][j]-1])
+	// initial replacement
+	initial_replace_matrix := sipherCode.GetInitialReplaceMatrix()
+	for i := 0; i < initial_siphercode_side; i++ {
+		for j := 0; j < initial_siphercode_side; j++ {
+			clear_text_init_replace += string(clear_text[initial_replace_matrix[i][j]-1])
 		}
 	}
-	L0 := clear_text_init_replace[:32]
-	L1 := clear_text_init_replace[32:]
-
+	// fmt.Println(clear_text_init_replace)
 	// Encryption processing
 	// PC-1(子密钥的生成)
-
+	for i := 1; i <= 16; i++ {
+		sipherCodeN := sipherCode.GetSipherCodeN(i)
+		fmt.Println(sipherCodeN)
+	}
 }
--- sipher_code/cipher_code.go
+++ sipher_code/cipher_code.go
@@ -1,40 +1,38 @@
 package sipher_code
 
-import (
-    "fmt"
-)
+var initial_sipher_code = "0001001100110100010101110111100110011011101111001101111111110001"
 
-var initial_sipher_code = [8][8]int{
-    {58, 50, 42, 34, 26, 18, 10, 2},
-    {60, 52, 44, 36, 28, 20, 12, 4},
-    {62, 54, 46, 38, 30, 22, 14, 6},
-    {64, 56, 48, 40, 32, 24, 16, 8},
-    {57, 49, 41, 33, 25, 17, 9, 1},
-    {59, 51, 43, 35, 27, 19, 11, 3},
-    {61, 53, 45, 37, 29, 21, 13, 5},
-    {63, 55, 47, 39, 31, 23, 15, 7},
+var initial_replace_matrix = [8][8]int{
+	{58, 50, 42, 34, 26, 18, 10, 2},
+	{60, 52, 44, 36, 28, 20, 12, 4},
+	{62, 54, 46, 38, 30, 22, 14, 6},
+	{64, 56, 48, 40, 32, 24, 16, 8},
+	{57, 49, 41, 33, 25, 17, 9, 1},
+	{59, 51, 43, 35, 27, 19, 11, 3},
+	{61, 53, 45, 37, 29, 21, 13, 5},
+	{63, 55, 47, 39, 31, 23, 15, 7},
 }
 
-var PC_1 = [56]int{
-    57, 49, 41, 33, 25, 17, 9,
-    1, 58, 50, 42, 34, 26, 18,
-    10, 2, 59, 51, 43, 35, 27,
-    19, 11, 3, 60, 52, 44, 36,
-    63, 55, 47, 39, 31, 23, 15,
-    7, 62, 54, 46, 38, 30, 22,
-    14, 6, 61, 53, 45, 37, 29,
-    21, 13, 5, 28, 20, 12, 4,
+var pc_1 = [56]int{
+	57, 49, 41, 33, 25, 17, 9,
+	1, 58, 50, 42, 34, 26, 18,
+	10, 2, 59, 51, 43, 35, 27,
+	19, 11, 3, 60, 52, 44, 36,
+	63, 55, 47, 39, 31, 23, 15,
+	7, 62, 54, 46, 38, 30, 22,
+	14, 6, 61, 53, 45, 37, 29,
+	21, 13, 5, 28, 20, 12, 4,
 }
 
-var PC_2 = [48]int{
-    14, 17, 11, 24, 1, 5,
-    3, 28, 15, 6, 21, 10,
-    23, 19, 12, 4, 26, 8,
-    16, 7, 27, 20, 13, 2,
-    41, 52, 31, 37, 47, 55,
-    30, 40, 51, 45, 33, 48,
-    44, 49, 39, 56, 34, 53,
-    46, 42, 50, 36, 29, 32
+var pc_2 = [48]int{
+	14, 17, 11, 24, 1, 5,
+	3, 28, 15, 6, 21, 10,
+	23, 19, 12, 4, 26, 8,
+	16, 7, 27, 20, 13, 2,
+	41, 52, 31, 37, 47, 55,
+	30, 40, 51, 45, 33, 48,
+	44, 49, 39, 56, 34, 53,
+	46, 42, 50, 36, 29, 32,
 }
 
 var displacements = [16]int{1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1}
@@ -42,43 +40,54 @@ var displacements = [16]int{1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1}
 type Sipher_code struct {
 }
 
-func (*Sipher_code) GetInitialSipherCode() [8][8]int {
-    return initial_sipher_code
+func (*Sipher_code) GetInitialReplaceMatrix() [8][8]int {
+	return initial_replace_matrix
 }
 
-func (*Sipher_code) getCN(round int) [28]int {
-    displacement := getDisplacements(round)
-    CN := PC_0_sipher_code[displacement:28]
-    for i := 0; i < displacement; i++ {
-        CN = append(CN, PC_0_sipher_code[i])
-    }
-    return CN
+func (*Sipher_code) getCN(round int) []int {
+	displacement := getDisplacements(round)
+	CN := pc_1[displacement:28]
+	for i := 0; i < displacement; i++ {
+		CN = append(CN, pc_1[i])
+	}
+	return CN
 }
 
-func (*Sipher_code) getDN(round int) [28]int {
-    displacement := getDisplacements(round)
-    DN := PC_0_sipher_code[28+displacement : 56]
-    for i := 0; i < displacement; i++ {
-        DN = append(DN, PC_0_sipher_code[28+i])
-    }
-    return DN
+func (*Sipher_code) getDN(round int) []int {
+	displacement := getDisplacements(round)
+	DN := pc_1[28+displacement:]
+	for i := 0; i < displacement; i++ {
+		DN = append(DN, pc_1[28+i])
+	}
+	return DN
 }
 
-func (sipher_code *Sipher_code) GetPCN(round int) [56]int {
-    PCN := sipher_code.getCN(round)
-    DN := sipher_code.getDN(round)
-    for _, ele := range DN {
-        PCN = append(PCN, ele)
-    }
-    return PCN
+func (sipher_code *Sipher_code) getCNDN(round int) []int {
+	CNDN := sipher_code.getCN(round)
+	DN := sipher_code.getDN(round)
+	for _, ele := range DN {
+		CNDN = append(CNDN, ele)
+	}
+	return CNDN
 }
 
-func ()
+func (*Sipher_code) replace(CNDN []int) []int {
+	CNDN_replace := make([]int, 0)
+	for i := 0; i < 48; i++ {
+		CNDN_replace = append(CNDN_replace, CNDN[pc_2[i]-1])
+	}
+	return CNDN_replace
+}
 
-func getDisplacements(round int) {
-    displacement := 0
-    for i := 0; i < round; i++ {
-        displacement += displacements[i]
-    }
-    return displacement
+func (sipher_code *Sipher_code) GetSipherCodeN(round int) []int {
+	CNDN := sipher_code.getCNDN(round)
+	return sipher_code.replace(CNDN)
+}
+
+func getDisplacements(round int) int {
+	displacement := 0
+	for i := 0; i < round; i++ {
+		displacement += displacements[i]
+	}
+	return displacement
 }
