9a5a60486d22ccdf607f3b6747880323bbbc9653
liuyh73 <15989067460@163.com>
2018-10-27 21:07:25 +0800

    add some func

--- des/utils.go
+++ des/utils.go
@@ -1,5 +1,10 @@
 package des
 
+import (
+	"encoding/hex"
+	"strconv"
+)
+
 func decimalToBinary(data int) string {
 	switch data {
 	case 0:
@@ -38,56 +43,52 @@ func decimalToBinary(data int) string {
 	return ""
 }
 
-func getRow(num1, num2 byte) int {
-	switch string(num1) + string(num2) {
-	case "00":
-		return 0
-	case "01":
-		return 1
-	case "10":
-		return 2
-	case "11":
-		return 3
+func hexadecimalToBinary(data byte) string {
+	switch data {
+	case '0':
+		return "0000"
+	case '1':
+		return "0001"
+	case '2':
+		return "0010"
+	case '3':
+		return "0011"
+	case '4':
+		return "0100"
+	case '5':
+		return "0101"
+	case '6':
+		return "0110"
+	case '7':
+		return "0111"
+	case '8':
+		return "1000"
+	case '9':
+		return "1001"
+	case 'a':
+		return "1010"
+	case 'b':
+		return "1011"
+	case 'c':
+		return "1100"
+	case 'd':
+		return "1101"
+	case 'e':
+		return "1110"
+	case 'f':
+		return "1111"
 	}
-	return -1
+	return ""
+}
+
+func getRow(num1, num2 byte) int {
+	row, _ := strconv.ParseInt(string(num1)+string(num2), 2, 64)
+	return int(row)
 }
 
 func getColumn(column string) int {
-	switch column {
-	case "0000":
-		return 0
-	case "0001":
-		return 1
-	case "0010":
-		return 2
-	case "0011":
-		return 3
-	case "0100":
-		return 4
-	case "0101":
-		return 5
-	case "0110":
-		return 6
-	case "0111":
-		return 7
-	case "1000":
-		return 8
-	case "1001":
-		return 9
-	case "1010":
-		return 10
-	case "1011":
-		return 11
-	case "1100":
-		return 12
-	case "1101":
-		return 13
-	case "1110":
-		return 14
-	case "1111":
-		return 15
-	}
-	return -1
+	col, _ := strconv.ParseInt(column, 2, 64)
+	return int(col)
 }
 
 func reverse(keys []string) []string {
@@ -96,4 +97,30 @@ func reverse(keys []string) []string {
 		keys_reverse = append(keys_reverse, keys[i])
 	}
 	return keys_reverse
+}
+
+func formatKey(key string) string {
+	hexKey := hex.EncodeToString([]byte(key))
+	binKey := ""
+	for i := 0; i < len(hexKey); i++ {
+		binKey += hexadecimalToBinary(hexKey[i])
+	}
+	return binKey
+}
+
+func binText(text string) string {
+	binText := ""
+	for i := 0; i < len(text); i++ {
+		binText += hexadecimalToBinary(text[i])
+	}
+	return binText
+}
+
+func hexText(text string) string {
+	hexText := ""
+	for i := 0; i < len(text)/4; i++ {
+		dec_text, _ := strconv.ParseInt(text[i*4:i*4+4], 2, 64)
+		hexText += strconv.FormatInt(dec_text, 16)
+	}
+	return hexText
 }
